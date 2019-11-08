04b2348893deaee9fc749bab074cdf1aca083290
Charan <56113587+Charan-MoJ@users.noreply.github.com>
2019-11-08 15:15:42 +0000

    Initial content commit
    - Content from two Libra lists
    - Date segmentation
    - Last updated
    - Pagination

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,16 @@ $(document).ready(function () {
     }
   });
 
-new MOJFrontend.SortableTable({
-table: $('table')[0]
-});
+// new MOJFrontend.SortableTable({
+// table: $('table')[0]
+// });
+
+
+var days = ["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "Friday",  "Saturday"]
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var n = new Date();
+var y = n.getFullYear();
+var m = n.getMonth();
+var d = n.getDate();
+var x = n.getDay();
+document.getElementById("date").innerHTML = days[x]+","+ " " + " "  + d + " " + months[m] + " " + y;

5ea884bca9ee992fedd14aeb6be7f55710786a79
Charan <56113587+Charan-MoJ@users.noreply.github.com>
2019-11-20 00:05:22 +0000

    Manchester, index, cards, add fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,28 @@ $(document).ready(function () {
 
 
 
+  new MOJFrontend.FilterToggleButton({
+    bigModeMediaQuery: '(min-width: 48.063em)',
+    startHidden: true,
+    toggleButton: {
+      container: $('.moj-action-bar__filter'),
+      showText: 'Show filter',
+      hideText: 'Hide filter',
+      classes: 'govuk-button--secondary'
+    },
+    closeButton: {
+      container: $('.moj-filter__header-action'),
+      text: 'Close'
+    },
+    filter: {
+      container: $('.moj-filter')
+    }
+  });
+
+
+
+
+
   new MOJFrontend.AddAnother($('.moj-add-another'));
 
 // new MOJFrontend.SortableTable({

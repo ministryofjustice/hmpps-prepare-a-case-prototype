a4874818b3ca73ef77e8ca4cb22cda98eb62d9a9
Paul Massey <paul.massey@scriptwerx.io>
2019-12-02 15:15:17 +0000

    Included basic filter UI
    Signed-off-by: Paul Massey <paul.massey@scriptwerx.io>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,53 +9,63 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
 
-  new MOJFrontend.SearchToggle({
-    toggleButton: {
-      container: $('.moj-search-toggle__toggle'),
-      text: 'Find case'
-    },
-    search: {
-      container: $('.moj-search')
-    }
-  });
+// Filters functionality
+function resetFilterUI () {
+  var filterButtons = document.getElementsByClassName('app-filter-button')
+  var filterSections = document.getElementsByClassName('app-filter-selection')
+  for (var i = 0, len = filterButtons.length; i < len; i++) {
+    filterButtons[i].classList.remove('app-filter-button--open')
+  }
+  for (var i2 = 0, len2 = filterSections.length; i2 < len2; i2++) {
+    filterSections[i2].classList.add('govuk-visually-hidden')
+  }
+}
 
+function openFilter ($el) {
+  resetFilterUI()
+  $el.classList.add('app-filter-button--open')
+  document.getElementById($el.dataset['controls']).classList.remove('govuk-visually-hidden')
+}
 
+new MOJFrontend.SearchToggle({
+  toggleButton: {
+    container: $('.moj-search-toggle__toggle'),
+    text: 'Find case'
+  },
+  search: {
+    container: $('.moj-search')
+  }
+})
 
+new MOJFrontend.FilterToggleButton({
+  bigModeMediaQuery: '(min-width: 48.063em)',
+  startHidden: true,
+  toggleButton: {
+    container: $('.moj-action-bar__filter'),
+    showText: 'Show filter',
+    hideText: 'Hide filter',
+    classes: 'govuk-button--secondary'
+  },
+  closeButton: {
+    container: $('.moj-filter__header-action'),
+    text: 'Close'
+  },
+  filter: {
+    container: $('.moj-filter')
+  }
+})
 
-  new MOJFrontend.FilterToggleButton({
-    bigModeMediaQuery: '(min-width: 48.063em)',
-    startHidden: true,
-    toggleButton: {
-      container: $('.moj-action-bar__filter'),
-      showText: 'Show filter',
-      hideText: 'Hide filter',
-      classes: 'govuk-button--secondary'
-    },
-    closeButton: {
-      container: $('.moj-filter__header-action'),
-      text: 'Close'
-    },
-    filter: {
-      container: $('.moj-filter')
-    }
-  });
-
-
-
-
-
-  new MOJFrontend.AddAnother($('.moj-add-another'));
+new MOJFrontend.AddAnother($('.moj-add-another'))
 
 // new MOJFrontend.SortableTable({
 // table: $('table')[0]
 // });
 
-
-var days = ["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "Friday",  "Saturday"]
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var n = new Date();
-var y = n.getFullYear();
-var m = n.getMonth();
-var d = n.getDate();
-var x = n.getDay();
-document.getElementById("date").innerHTML = days[x]+","+ " " + " "  + d + " " + months[m] + " " + y;
+var days = ['Sunday', 'Monday', 'Teusday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var n = new Date()
+var y = n.getFullYear()
+var m = n.getMonth()
+var d = n.getDate()
+var x = n.getDay()
+document.getElementById('date').innerHTML = days[x] + ',' + ' ' + ' ' + d + ' ' + months[m] + ' ' + y

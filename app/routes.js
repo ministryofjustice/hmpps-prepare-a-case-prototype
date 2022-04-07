39ca4d0bcbefbb5f9818853f4eeb21c7963cf29d
Jon Wyatt <jon.wyatt@digital.justice.gov.uk>
2022-04-07 16:06:03 +0100

    Drag & drop document upload (case 12)

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const uploadRecallDocuments = require('./views/cases/12/uploadDocuments/recallUploadDocuments')
 
 // Add your routes here - above the module.exports line
+
+router.get('/cases/12/upload_documents_drag_drop', uploadRecallDocuments.get)
+router.post('/cases/12/upload_documents_drag_drop', uploadRecallDocuments.post)
 
 module.exports = router
---
+++ app/views/cases/12/uploadDocuments/documentTypes.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    label: "CPS pack",
+    category: "CPS_PACK",
+    type: "document",
+    fileName: "Part A.pdf",
+  },
+  {
+    label: "DV logs",
+    category: "DV_LOGS",
+    type: "document",
+    fileName: "Licence.pdf",
+  },
+  {
+    label: "Unclassified",
+    category: "UNCLASSIFIED",
+    type: "document",
+  }
+];
